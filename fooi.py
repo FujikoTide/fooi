7cfed002958131c436da6aac8ef9768ffdce2dfa
Robin Mukherjee <robin@otaking.co.uk>
2024-12-04 16:49:22 +0000

    first commit

---
+++ fooi.py
@@ -0,0 +1,109 @@
+#
+#   Find Orphaned Obsidian Images
+#
+
+# TODO:
+# add command line functionality
+# add error checking where needed
+# add dry run mode
+
+import json
+from pathlib import Path
+import re
+
+DELETEDIR = "DELETE"
+
+
+def main():
+    vaultDir = Path(r"")
+    deleteDir = Path(vaultDir / DELETEDIR)
+    fileList = getFileList(vaultDir)
+    imageLinkList = getImageLinkList(fileList)
+    imageFileList = getImageFileList(vaultDir)
+    # make deletion list a function I suppose?
+    deletionList = []
+    for imageFile in imageFileList:
+        file = Path(imageFile)
+        if file.name not in imageLinkList:
+            deletionList.append(file.name)
+    logFiles(deleteDir, deletionList)
+    moveFiles(vaultDir, deleteDir, deletionList)
+    printFiles(deletionList)
+
+
+def logFiles(deleteDir, deletionList):
+    try:
+        deleteDir.mkdir()
+        print(f"Directory {deleteDir} created successfully.")
+    except FileExistsError:
+        print(f"Directory {deleteDir} already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create {deleteDir}.")
+    except Exception as e:
+        print(f"An error occured: {e}")
+
+    with Path(deleteDir / "filesForDeletion.json").open("w", encoding="utf-8") as deletionFile:
+        json.dump(deletionList, deletionFile, indent=4, ensure_ascii=False)
+
+
+def moveFiles(vaultDir, deleteDir, deletionList):
+    for file in deletionList:
+        source = vaultDir / file
+        destination = deleteDir / file
+     
+        try:
+            with destination.open(mode="xb") as file:
+                file.write(source.read_bytes())
+        except FileExistsError:
+            print(f"File {destination} already exists.")
+        else:
+            source.unlink()
+
+
+def printFiles(deletionList):
+    print("Files to be deleted (moved to DELETE folder):\n")
+    for file in deletionList:
+        print(file)
+    
+
+def getFileList(vaultDir):
+    return list(vaultDir.rglob("*.md"))
+
+
+def getImageLinkList(fileList):
+    imageList = []
+    for file in fileList:
+        with open(file, 'r', encoding="utf-8") as f:
+            match = re.findall("\[\[(.+?(png|jpg|pdf)).*?]]", f.read())
+            if len(match) > 0:
+                for image in match:
+                    imageList.append(image[0])
+    
+    return imageList
+
+# functionalise the list stuff
+def getImageFileList(vaultDir):
+    imageFileList = []
+    # must be an iterable for isdisjoint to work
+    skipDir = [DELETEDIR]
+    
+    pngList = [item for item in vaultDir.rglob("*.png") if set(item.parts).isdisjoint(skipDir)]
+    if len(pngList) > 0:
+        for file in pngList:
+            imageFileList.append(file)
+
+    jpgList = [item for item in vaultDir.rglob("*.jpg") if set(item.parts).isdisjoint(skipDir)]
+    if len(jpgList) > 0:
+        for file in jpgList:
+            imageFileList.append(file)
+
+    pdfList = [item for item in vaultDir.rglob("*.pdf") if set(item.parts).isdisjoint(skipDir)]
+    if len(pdfList) > 0:
+        for file in pdfList:
+            imageFileList.append(file)
+
+    return imageFileList
+
+  
+if __name__ == "__main__":
+    main()
